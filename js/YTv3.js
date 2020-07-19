4e17d2c91e1c0cf306d4f094a44254f00ebc0344
harshaabeyvickrama <harshaabeyvickrama@gmail.com>
2020-07-19 12:49:29 +0530

    fghfgh

--- js/YTv3.js
+++ js/YTv3.js
@@ -91,9 +91,10 @@ function getCommentsForVideo(videoId) {
     "videoId": videoId
   })
       .then(function(response) {
-             dat=response;
+             dat=response.result.items;
               // Handle the results here (response.result has the parsed body).
               console.log("Response", dat);
+              console.log("Response", dat.lenght);
               if(dat==null){
                 console.log("error......")
               }
@@ -112,4 +113,4 @@ gapi.load("client:auth2", function() {
   function s(){
     channelDetails("UCNIPltykIATy0PhRp82uNMQ");
   }
-console.log("YTV3 update 6"); 
+console.log("YTV3 update 7"); 
